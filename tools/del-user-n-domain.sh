69c3388451be3f7d8592131a536115444eb19d9c
Vasily Asakasinsky <asakasinsky@gmail.com>
2014-01-11 17:28:08 +0300

    Update del-user-n-domain.sh

--- tools/del-user-n-domain.sh
+++ tools/del-user-n-domain.sh
@@ -6,16 +6,21 @@ read ROOTPASS;
 echo "Enter username to delete:";
 read USERNAME;
 
+
+cd /home/$USERNAME/workspace
+
 for directory in `find /home/$USERNAME/workspace -type d`
 do
     echo "delete $directory"
     rm -f "/etc/nginx/sites-enabled/$directory.conf";
     rm -f "/etc/nginx/sites-available/$directory.conf";
     rm -f "/etc/php5/fpm/pool.d/$directory.conf";
-    rm -f "/etc/php5/fpm/pool.d/$directory.conf";
     # find /var/log/nginx/ -type f -name "$USERNAME*" -exec rm '{}';
     # find /var/log/nginx/ -type f -name "$directory*" -exec rm '{}';
 done
+
+cd
+
 mysql -uroot --password=$ROOTPASS -e "DROP USER $USERNAME@localhost";
 mysql -uroot --password=$ROOTPASS -e "DROP DATABASE $USERNAME";
 service nginx reload;
