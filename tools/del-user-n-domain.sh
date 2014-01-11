0c3e6da1d0701b8561cf82dc6a63bfe9ca44d3de
Vasily Asakasinsky <asakasinsky@gmail.com>
2014-01-11 19:32:36 +0300

    Update del-user-n-domain.sh

--- tools/del-user-n-domain.sh
+++ tools/del-user-n-domain.sh
@@ -8,7 +8,7 @@ read USERNAME;
 
 
 /etc/init.d/nginx stop;
-/etc/init.d/php5-fpm stop;
+service php5-fpm stop;
 
 cd "/home/$USERNAME/workspace"
 
@@ -17,7 +17,7 @@ do
     if [ -d "$directory" ];then
         rm -f "/etc/nginx/sites-enabled/$directory.conf";
         rm -f "/etc/nginx/sites-available/$directory.conf";
-        rm -f "/etc/php5/fpm/pool.d/$directory.conf";
+        rm -f "/etc/php5/fpm/pool.d/$directory.pool.conf";
         # find /var/log/nginx/ -type f -name "$USERNAME*" -exec rm '{}';
         # find /var/log/nginx/ -type f -name "$directory*" -exec rm '{}';
       fi
@@ -29,6 +29,6 @@ mysql -uroot --password=$ROOTPASS -e "DROP USER '$USERNAME'@'localhost'";
 mysql -uroot --password=$ROOTPASS -e "DROP DATABASE \`$USERNAME\`";
 
 /etc/init.d/nginx start;
-/etc/init.d/php5-fpm start;
+service php5-fpm start;
 
 userdel -rf $USERNAME;
